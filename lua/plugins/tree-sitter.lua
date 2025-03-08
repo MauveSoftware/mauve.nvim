661452175b04c49836b508ea5ecb4002a5469879
Daniel Brendgen-Czerwonk <daniel@routing.rocks>
2025-03-08 11:26:27 +0100

    no auto install for tree-sitter parsers

--- lua/plugins/tree-sitter.lua
+++ lua/plugins/tree-sitter.lua
@@ -1,19 +1,5 @@
 require('nvim-treesitter.configs').setup {
-  ensure_installed = {
-    "bash",
-    "cmake",
-    "dockerfile",
-    "hcl",
-    "json",
-    "lua",
-    "markdown",
-    "markdown_inline",
-    "perl",
-    "query",
-    "ruby",
-    "yaml",
-  },
-  auto_install = true,
+  auto_install = false,
   parser_install_dir = os.getenv('HOME') .. '/.nvim/tree-sitter/parsers',
   highlight = {
     enable = true,
