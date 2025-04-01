7b13ae23026929fe744fe492e2fecb7a272ffdb4
Daniel Brendgen-Czerwonk <daniel@routing.rocks>
2025-04-01 16:36:33 +0200

    cleanup unused key bindings

--- init.lua
+++ init.lua
@@ -44,20 +44,17 @@ vim.opt.undodir = os.getenv('HOME') .. '/.nvim/undodir'
 vim.opt.undofile = true
 
 -- convenience key mappings
-vim.keymap.set('n', 'Y', 'Y', { desc = 'Yank line' }) -- restore yank behavior of VIM
+vim.keymap.set('n', 'Y', 'yy', { desc = 'Yank line' })
+vim.keymap.set({'n', 'v'}, '<leader>y', [["+y]], { desc = 'Yank to clipbaord' })
+vim.keymap.set('n', '<leader>Y', [["+Y]], { desc = 'Yank line to clipbaord' })
 vim.keymap.set('n', '<C-d>', '<C-d>zz', { desc = 'Scroll down' }) -- reposition after scrolling down
 vim.keymap.set('n', '<C-u>', '<C-u>zz', { desc = 'Scroll up' }) -- reposition after scrolling up
 vim.keymap.set('n', 'n', 'nzzzv') -- reposition after junp to match
 vim.keymap.set('n', 'N', 'Nzzzv') -- reposition after junp to match
 vim.keymap.set('n', 'Q', '@qj', { desc = 'Apply Macro' })
 vim.keymap.set('x', 'Q', ':norm @q<CR>', { desc = 'Apply Macro' })
-vim.keymap.set({'n', 'v'}, '<leader>y', [["+y]], { desc = 'Yank to Clipbaord' })
-vim.keymap.set('n', '<leader>Y', [["+Y]], { desc = 'Yank to Clipbaord' })
 vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv", { desc = 'Move down' })
 vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv", { desc = 'Move up'})
-vim.keymap.set('n', '<leader>qc', '<cmd>copen<CR>', { desc = 'Open quickfix list' })
-vim.keymap.set('n', '<leader>qn', '<cmd>cnext<CR>', { desc = 'Next quickfix list item' })
-vim.keymap.set('n', '<leader>qp', '<cmd>cprev<CR>', { desc = 'Previous quickfix list item' })
 vim.keymap.set('n', '<c-b>', '<cmd>bnext<CR>', { desc = 'Switch to next buffer' })
 vim.keymap.set('n', '<leader>H', function()
   vim.cmd('%!xxd')
