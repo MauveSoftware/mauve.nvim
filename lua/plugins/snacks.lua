ad7e120d24bedbdc35128c78d518eb2f47680201
Daniel Brendgen-Czerwonk <daniel@routing.rocks>
2025-04-07 09:30:21 +0200

    add simple marks keymap setup

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -31,6 +31,7 @@ vim.keymap.set('n', '<leader>n', function() Snacks.notifier.show_history() end, 
 vim.keymap.set('n', '<C-n>', function() Snacks.explorer() end, { desc = 'Toggle Explorer' })
 vim.keymap.set('n', '<leader>u', function() Snacks.picker.undo() end, { desc = 'Undo Picker' })
 vim.keymap.set('n', '<leader>i', function() Snacks.picker.icons() end, { desc = 'Icon Picker' })
+vim.keymap.set('n', '<leader>mm', function() Snacks.picker.marks() end, { desc = 'Mark Picker' })
 vim.keymap.set('n', '<leader>fb', function() Snacks.picker.buffers() end, { desc = 'Buffers' })
 vim.keymap.set('n', '<leader>fc', function() Snacks.picker.commands() end, { desc = 'Commands' })
 vim.keymap.set('n', '<leader>ff', function() Snacks.picker.files() end, { desc = 'Files' })
