f5a03ba77ed1a8b25847ef1aae699ef2e6ca6ef4
Daniel Brendgen-Czerwonk <daniel@routing.rocks>
2025-04-13 10:33:17 +0200

    improve folding

---
+++ lua/config/folding.lua
@@ -0,0 +1,5 @@
+vim.opt.foldenable = true
+vim.opt.foldmethod = "indent"
+vim.opt.foldnestmax = 3
+vim.opt.foldlevel = 99
+vim.opt.foldminlines = 3
--- lua/plugins/tree-sitter.lua
+++ lua/plugins/tree-sitter.lua
@@ -8,4 +8,13 @@ require('nvim-treesitter.configs').setup {
   indent = {
     enable = true,
   },
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = '<c-space>',
+      node_incremental = '<c-space>',
+      scope_incremental = false,
+      node_decremental = "<bs>",
+    },
+  },
 }
